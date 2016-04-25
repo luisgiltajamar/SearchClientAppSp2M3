ba441d6dd9dcec2afa3c838a35b08b04b3f036a3
Luis Felipe Gil <lamaslg@gmail.com>
2016-04-25 13:14:45 +0200

    fin

---
+++ SearchClientApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SearchClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearchTerm = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearchTerm
+            // 
+            this.txtSearchTerm.Location = new System.Drawing.Point(25, 24);
+            this.txtSearchTerm.Name = "txtSearchTerm";
+            this.txtSearchTerm.Size = new System.Drawing.Size(260, 20);
+            this.txtSearchTerm.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(301, 24);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(900, 400);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 550);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchTerm);
+            this.Name = "Form1";
+            this.Text = "Client Search Application";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearchTerm;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ SearchClientApp/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.Search;
+using Microsoft.SharePoint.Client.Search.Query;
+
+namespace SearchClientApp
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public DataTable ConvertToDataTable(ResultTable rt)
+        {
+            DataTable queryDataTable = new DataTable();
+
+            try
+            {
+                IDictionary<string, object> r = rt.ResultRows.ToArray()[0];
+                foreach (string id in r.Keys)
+                {
+                    queryDataTable.Columns.Add(id);
+                }
+                foreach (var row in rt.ResultRows)
+                {
+                    DataRow dr = queryDataTable.NewRow();
+                    foreach (DataColumn dc in queryDataTable.Columns)
+                    {
+                        dr[dc.ColumnName] = row[dc.ColumnName];
+                    }
+                    queryDataTable.Rows.Add(dr);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return queryDataTable;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            this.btnSearch.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+
+            var searchResults = Task.Factory.StartNew(() =>
+            {
+                ClientContext clientContext = new ClientContext("http://intranet.contoso.com");
+                KeywordQuery keywordQuery = new KeywordQuery(clientContext);
+                keywordQuery.QueryText = txtSearchTerm.Text;
+
+                DataTable queryDataTable = null;
+                SearchExecutor searchExecutor = new SearchExecutor(clientContext);
+                ClientResult<ResultTableCollection> results = searchExecutor.ExecuteQuery(keywordQuery);
+                clientContext.ExecuteQuery();
+
+                foreach (ResultTable resultsTable in results.Value)
+                {
+                    queryDataTable = ConvertToDataTable(resultsTable);
+                }
+
+                return queryDataTable;
+            });
+
+            this.dataGridView1.DataSource = await searchResults;
+            this.dataGridView1.Refresh();
+            this.btnSearch.Enabled = true;
+            this.Cursor = Cursors.Default;
+        }
+    }
+}
